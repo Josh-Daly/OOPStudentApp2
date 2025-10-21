762fe0a7b9b66b023288cd4d953881a2a60cbc50
Josh-Daly <g00470372@atu.ie>
2025-10-21 12:29:18 +0100

    Initial commit

---
+++ src/main/java/ie/atu/week6/StudentApp.java
@@ -0,0 +1,14 @@
+//Paul Daly, OOP Lab 3, StudentApp recreated + Homework from Lab 4
+
+
+package ie.atu.week6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class StudentApp {
+    public static void main(String[] args) {
+        
+    }
+}
